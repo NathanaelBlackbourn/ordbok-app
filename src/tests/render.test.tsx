c169b42736467dab4904b060226a55d98f3c07b6
NathanaelBlackbourn <nathanael.blackbourn@hoy.se>
2023-12-01 09:28:44 +0100

    Test search bar render

--- src/tests/render.test.tsx
+++ src/tests/render.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from 'src/App';
 
 describe('initial load and render', () => {
@@ -8,5 +9,14 @@ describe('initial load and render', () => {
         expect(
             await screen.findByText('Search the Dictionary')
         ).toBeInTheDocument();
+    });
+
+    it('should render a functional search bar', async () => {
+        render(<App />);
+        const user = userEvent.setup();
+
+        expect(await screen.findByRole('textbox')).toBeInTheDocument();
+        await user.type(screen.getByRole('textbox'), 'test');
+        expect(screen.getByRole('textbox')).toHaveValue('test');
     });
 });
